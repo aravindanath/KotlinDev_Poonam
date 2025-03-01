20549d6eaa298a67cf2e29582985f2c1790bb7b2
poonampgaikwad <poonampgaikwad@gmail.com>
2025-03-01 10:27:01 -0600

    git commit

--- src/main/kotlin/Poonam_Practice/day5/ListExample4.kt
+++ src/main/kotlin/Poonam_Practice/day5/ListExample4.kt
@@ -1,11 +1,27 @@
 package org.example.Poonam_Practice.day5
 
 fun main(){
-    var country = mutableListOf("India","USA","UK")
+    var country = mutableListOf("India","USA","UK",null)
     country.add("Mexico")
 
-    println(country.sorted().sortedDescending())
+    //println(country.sorted().sortedDescending())
+
+    if("India" in country)
+    {
+        println("Asia")
+    }
+
+    else{
+        println("other country")
+    }
 
 
+println(country.indexOf("India"))
+    println("Element at 2nd position is :"+country.get(1))
 
+    println("remove null from list"+ country.filterNotNull())
+    println(country)
+
+    var elements = country.drop(2)
+    println(elements)
 }
